0bac8e4a3593073e83439228fd867ee33bd9c194
terribleted65 <tedth@amazon.com>
2014-10-17 06:20:12 -0700

    Reproducible Research Assessment 1
    Coursera Reproducible Research Project Assessment 1.  Using knitr

---
+++ PA1_template.R
@@ -0,0 +1,79 @@
+# read in full activity csv file
+activity_all <- read.csv(file="./activity.csv", header=TRUE)
+#calculate daily steps
+library(plyr)
+daily_steps <- ddply(activity_all, .(date), summarize, t_steps=sum(steps,na.rm=TRUE))
+# print histogram of steps taken per day
+hist(daily_steps$t_steps, main="Number of Daily Steps", xlab="Steps Taken", 
+     ylab="Frequency", col="green", breaks=30, xlim=c(0,25000), ylim=c(0,12))
+# mean number of steps per day
+mean(daily_steps$t_steps)
+# median number of steps per day
+median(daily_steps$t_steps)
+
+#question 2
+#calc average # of steps per interval across days
+avg_steps_interval <- aggregate(activity_all$steps, by=list(activity_all$interval),
+    FUN=mean, na.rm=TRUE)
+colnames(avg_steps_interval)[1] <- "interval"
+colnames(avg_steps_interval)[2] <- "avg_steps"
+#create time series plot for avg steps by interval
+library(ggplot2)
+qplot(interval,avg_steps, data=avg_steps_interval,geom="line",xlab="5 Minute Intervals",
+      ylab="Average Number of Steps")
+#find time interval has the max avg number of steps
+max_avg_steps_row <- which.max(avg_steps_interval$avg_steps)
+avg_steps_interval[max_avg_steps_row,1]
+#
+#Question 3
+# calculate the number of rows with NA values
+total_rows <- NROW(activity_all)
+good_rows <- NROW(na.omit(activity_all))
+total_rows - good_rows
+# impute missing values by using the average number of steps for an interval as
+# the imputed value
+activity_w_values <- activity_all
+for (n in 1:NROW(activity_w_values)){
+    if (is.na(activity_w_values$steps[n])){
+        activity_w_values$steps[n] <- 
+        avg_steps_interval$avg_steps[which(activity_w_values$interval[n]==avg_steps_interval$interval)]
+    }
+}
+# calculate daily steps with imputed values included
+daily_steps_imp <- ddply(activity_w_values, .(date), summarize, t_steps=sum(steps))
+# print histogram of steps taken per day with values imputed
+hist(daily_steps_imp$t_steps, main="Number of Daily Steps", xlab="Steps Taken", 
+     ylab="Frequency", col="green", breaks=30, xlim=c(0,25000), ylim=c(0,12))
+# mean number of steps per day
+mean(daily_steps_imp$t_steps)
+# median number of steps per day
+median(daily_steps_imp$t_steps)
+# question 4
+activity_w_values$day <- weekdays(as.Date(activity_w_values$date))
+# determine weekday or weekend
+activity_w_values$day <- ifelse ((activity_w_values$day== "Saturday" | activity_w_values$day== "Sunday"), "Weekend", "Weekday")
+# average steps by day#####################
+averages <- aggregate(steps ~ interval + day, data = activity_w_values, mean)
+# plot 
+ggplot(averages, aes(interval, steps)) + geom_line() + facet_grid(day ~ .) + 
+    xlab("5-minute interval") + ylab("Number of steps")
+#
+#
+#
+## testing code
+#
+#daily_steps$t_steps[is.na(daily_steps$t_steps)] <- 0
+# remove NA data
+#good_activity <- activity_all[complete.cases(activity_all),]
+#daily_steps$t_steps[is.na(daily_steps$t_steps)] <- 0
+# calc total steps per day
+#daily_steps <- aggregate(activity_all, by=list(activity_all$date,activity_all$steps), FUN=sum, na.rm=TRUE)
+# set date column format
+#activity_all$date <- as.Date(activity_all$date,"%Y-%m-%d")
+
+library(ggplot2)
+qplot(t_steps, data=daily_steps, geom="histogram")
+
+
+hist(v,main="Number of Daily Steps", 
+     xlab="Steps", ylab="Frequency", col="Red")
---
+++ PA1_template.md
@@ -0,0 +1,67 @@
+---
+title: "PA1_template.Rmd"
+author: "terribleted65"
+date: "Wednesday, October 15, 2014"
+output: html_document
+---
+Analyze, graph, and report on the activity of an anonymous individual using 2 months 
+of data gathered from their personal activity monitoring device (e.g. fitbit).
+
+Load the data
+
+
+```r
+activity_all <- read.csv(file="./activity.csv", header=TRUE)
+```
+
+
+The total number of steps taken each day while ignoring missing data.
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-1.png) 
+
+The mean number of steps per day:
+
+```
+## [1] 9354.23
+```
+
+The median number of steps per day:
+
+```
+## [1] 10395
+```
+
+Average number of steps taken across all days within a 5 minute interval while 
+ignoring missing values:
+
+![plot of chunk unnamed-chunk-6](figure/unnamed-chunk-6-1.png) 
+
+What time 5-minute time interval, on average, contains the maximum number of steps?
+
+
+```
+## [1] 835
+```
+
+Impute the missing steps data from the original data:
+
+
+```r
+# impute missing values by using the average number of steps for an interval as
+# the imputed value
+activity_w_values <- activity_all
+for (n in 1:NROW(activity_w_values)){
+    if (is.na(activity_w_values$steps[n])){
+        activity_w_values$steps[n] <- 
+        avg_steps_interval$avg_steps[which(activity_w_values$interval[n]==avg_steps_interval$interval)]
+    }
+}
+```
+
+Total number of steps taken each days using imputed values:
+
+![plot of chunk unnamed-chunk-9](figure/unnamed-chunk-9-1.png) 
+
+Compare the average number of steps taken per 5-minute interval across weekdays
+and weekends:
+
+![plot of chunk unnamed-chunk-10](figure/unnamed-chunk-10-1.png) 
